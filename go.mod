142377883006fbc5e7131c107cea4bf8f65d4d9e
Sameer Chaudhari <sameer.c77@gmail.com>
2023-01-06 17:18:26 +0530

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/sameerc77/godemo
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/sameerc77/godemo/math"
+)
+
+func main() {
+	fmt.Println((math.Add(3, 4)))
+}
